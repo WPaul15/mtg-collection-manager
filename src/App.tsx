9298b6e3f8c7983099f676ed055c65eb1557a96e
WPaul15 <willem.m.paul@gmail.com>
2022-10-02 16:56:12 -0500

    Use double quotes in JSX

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,27 @@ function App() {
   }
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Welcome to Tauri!</h1>
 
-      <div className='row'>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo vite' alt='Vite logo' />
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-        <a href='https://tauri.app' target='_blank'>
-          <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-      <div className='row'>
+      <div className="row">
         <div>
-          <input id='greet-input' onChange={(e) => setName(e.currentTarget.value)} placeholder='Enter a name...' />
-          <button type='button' onClick={() => greet()}>
+          <input id="greet-input" onChange={(e) => setName(e.currentTarget.value)} placeholder="Enter a name..." />
+          <button type="button" onClick={() => greet()}>
             Greet
           </button>
         </div>
