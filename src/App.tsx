56258ed0df8144c843edfcd53794773735567dc3
WPaul15 <willem.m.paul@gmail.com>
2022-12-24 13:23:46 -0800

    Use PrimeReact components in form

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Form, Formik } from 'formik';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import PrimeReact from 'primereact/api';
+import { Button } from 'primereact/button';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import * as Yup from 'yup';
@@ -18,7 +19,7 @@ function App() {
         oracleText: '',
       }}
       validationSchema={Yup.object({
-        cardName: Yup.string().required('Required'),
+        cardName: Yup.string(),
         oracleText: Yup.string(),
       })}
       onSubmit={(values, { setSubmitting }) => {
@@ -29,10 +30,12 @@ function App() {
       }}
     >
       <Form>
-        <TextInput label="Card Name" name="cardName" />
-        <TextInput label="Oracle Text" name="oracleText" />
+        <div className="formgrid grid">
+          <TextInput label="Card Name" name="cardName" />
+          <TextInput label="Oracle Text" name="oracleText" />
+        </div>
 
-        <button type="submit">Submit</button>
+        <Button type="submit" label="Submit" />
       </Form>
     </Formik>
   );
--- src/components/form/CustomInputs/TextInput.tsx
+++ src/components/form/CustomInputs/TextInput.tsx
@@ -1,24 +1,28 @@
 import { FieldHookConfig, useField } from 'formik';
-import { ClassAttributes, InputHTMLAttributes } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { classNames } from 'primereact/utils';
 
 interface TextInputProps {
   label: string;
+  columnWidth?: number;
 }
 
-export const TextInput = ({
-  label,
-  ...props
-}: TextInputProps &
-  InputHTMLAttributes<HTMLInputElement> &
-  ClassAttributes<HTMLInputElement> &
-  FieldHookConfig<string>) => {
+export const TextInput = ({ label, columnWidth, ...props }: TextInputProps & FieldHookConfig<string>) => {
   const [field, meta] = useField(props);
 
+  const id: string = props.id || props.name;
+  const isInvalid = !!(meta.touched && meta.error);
+
   return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input {...field} {...props} type="text" />
-      {meta.touched && meta.error ? <div>{meta.error}</div> : null}
-    </>
+    <div className={`field col-${columnWidth || 12} mb-4`}>
+      <label htmlFor={id}>{label}</label>
+      {/* TODO: Resolve type conflict for 'ref' prop */}
+      <InputText {...field} {...props} type="text" className={classNames({ 'p-invalid': isInvalid }, 'w-full')} />
+      {isInvalid && (
+        <small id={`${id}-error`} className="p-error block">
+          {meta.error}
+        </small>
+      )}
+    </div>
   );
 };
