e988f61a757109566ebab870e340aada5901b42e
WPaul15 <willem.m.paul@gmail.com>
2023-11-13 18:10:44 -0600

    Add PrimeReact

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { invoke } from "@tauri-apps/api/tauri";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+
 import "./App.css";
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import { PrimeReactProvider } from "primereact/api";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+
+import "primeflex/primeflex.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/soho-light/theme.css";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <PrimeReactProvider
+      value={{
+        ripple: true,
+      }}
+    >
+      <App />
+    </PrimeReactProvider>
+  </React.StrictMode>
 );
